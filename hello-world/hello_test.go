ca734792bb035ff691de2593915363247afccdc3
Pedro Pretto <pedropretto18@gmail.com>
2024-06-07 19:28:00 -0300

    Hello World: refatorados testes e adcionado caso padrao

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Pedro")
-	want := "Hello, Pedro"
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Pedro")
+		want := "Hello, Pedro"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("Got %q, want %q", got, want)
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
